c3868f9f973169a161aa0528479135b88e088b54
Aidan Crowther <aidan.crowther@outlook.com>
2022-09-25 15:25:46 -0400

    Added header and message generator helper functions

---
+++ helpers/header_generator.py
@@ -0,0 +1,17 @@
+id = 1
+points = 32
+home = 1
+enable = 1
+
+
+message = ((id << 28 )& 0xF0000000) | ((points << 20) & 0x0FF00000) | ((home << 19) & 0x000080000) | ((enable << 18) & 0x00040000)
+
+sum = message ^ (message >> 1)
+sum = sum ^ (sum >> 2)
+sum = sum ^ (sum >> 4)
+sum = sum ^ (sum >> 8)
+sum = sum ^ (sum >> 16)
+
+message = message ^ (sum & 1)
+
+print(hex(message))
---
+++ helpers/message_generator.py
@@ -0,0 +1,18 @@
+x = 122
+y = 92
+
+r = 7
+g = 0
+b = 0
+
+message = ((x << 23 )& 0xFF800000) | ((y << 14) & 0x007FC000) | ((r << 11) & 0x00003800) | ((g << 8) & 0x00000700) | ((b << 5) & 0x000000E0)
+
+sum = message ^ (message >> 1)
+sum = sum ^ (sum >> 2)
+sum = sum ^ (sum >> 4)
+sum = sum ^ (sum >> 8)
+sum = sum ^ (sum >> 16)
+
+message = message ^ (sum & 1)
+
+print(hex(message))
